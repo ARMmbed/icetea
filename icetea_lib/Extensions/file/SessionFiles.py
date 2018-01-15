a52a860e3a35d968b9ae8f2e3d044c841ff873bc
Joonas Nikula <joonas.nikula@arm.com>
2018-01-15 13:17:21 +0200

    Removed deprecated Extensions and ExtApps packages.

--- icetea_lib/Extensions/file/SessionFiles.py
+++
@@ -1,9 +0,0 @@
-from icetea_lib.tools.file.SessionFiles import JsonFile
-
-"""
-This file exists for backwards compatibility. the file module has been relocated
-to icetea_lib.tools but some other applications import if from this location.
-
-Backwards compatibility achieved by importing the contents of the module from the new
-location to this module.
-"""
