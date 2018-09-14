59c9a77f732fca17cca8e8c7cee0df026b3dea58
Jussi Vatjus-Anttila <jussi.vatjus-anttila@arm.com>
2018-09-14 10:32:53 +0300

    add python 3.5 to classifiers

--- setup.py
+++ setup.py
@@ -88,6 +88,7 @@ setup(name="icetea",
       install_requires=INSTALL_REQUIRES,
       classifiers=[
           "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: 3.7",
           "License :: OSI Approved :: Apache Software License",
