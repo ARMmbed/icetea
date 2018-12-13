b90a07062ec888578607e08f5e5711df2da2ff62
Joonas Nikula <joonas.nikula@arm.com>
2018-12-13 08:51:35 +0200

    Added store_traces property to Dut object for controlling in-memory storing of lines.

--- test/test_dut.py
+++ test/test_dut.py
@@ -19,13 +19,14 @@ import unittest
 
 import mock
 
+from icetea_lib.CliResponse import CliResponse
 from icetea_lib.DeviceConnectors.Dut import Dut
 from icetea_lib.TestStepError import TestStepError, TestStepTimeout
 
 
+@mock.patch("icetea_lib.DeviceConnectors.Dut.LogManager")
 class DutTestcase(unittest.TestCase):
 
-    @mock.patch("icetea_lib.DeviceConnectors.Dut.LogManager")
     def test_executionready_wait_skip(self, mock_log):  # pylint: disable=unused-argument
         dut = Dut("test_dut")
         with mock.patch.object(dut, "response_received") as mock_r:
@@ -43,7 +44,6 @@ class DutTestcase(unittest.TestCase):
             with self.assertRaises(TestStepError):
                 dut._wait_for_exec_ready()  # pylint: disable=protected-access
 
-    @mock.patch("icetea_lib.DeviceConnectors.Dut.LogManager")
     def test_execready_wait_timeout(self, mock_log):  # pylint: disable=unused-argument
         dut = Dut("test_dut")
         with mock.patch.object(dut, "response_received") as mock_r:
@@ -55,7 +55,6 @@ class DutTestcase(unittest.TestCase):
                 with self.assertRaises(TestStepTimeout):
                     dut._wait_for_exec_ready()  # pylint: disable=protected-access
 
-    @mock.patch("icetea_lib.DeviceConnectors.Dut.LogManager")
     def test_initclihuman(self, mock_log):  # pylint: disable=unused-argument
         dut = Dut("test_Dut")
         with mock.patch.object(dut, "execute_command") as m_com:
@@ -76,6 +75,51 @@ class DutTestcase(unittest.TestCase):
             self.assertListEqual(dut.post_cli_cmds, [["com1", True, False]])
             self.assertEqual(len(m_com.mock_calls), 1)
             m_com.assert_called_once_with("com1", wait=False, asynchronous=True)
+
+    def test_dut_run(self, mock_log):
+        dut = Dut("test_Dut")
+        type(dut)._logger = mock.MagicMock()
+        dut.logger = mock.MagicMock()
+        semaphore = mock.MagicMock()
+        semaphore.acquire = mock.MagicMock(side_effect=[True, Exception])
+
+        type(dut)._sem = semaphore
+        type(dut)._signalled_duts = [dut]
+        type(dut)._run = mock.MagicMock(side_effect=[True, False])
+        type(dut).waiting_for_response = mock.PropertyMock(return_value=None)
+
+        query = mock.MagicMock()
+        query.cmd = mock.MagicMock()
+        dut.query = query
+        dut.writeline = mock.MagicMock(side_effect=[Exception])
+        dut.readline = mock.MagicMock(return_value=False)
+        dut.response_received = mock.MagicMock()
+        with self.assertRaises(Exception):
+            dut.run()
+
+    def test_store_traces(self, mock_log):
+        dut = Dut("test")
+        self.assertTrue(dut.store_traces)
+        dut.store_traces = False
+        self.assertFalse(dut.store_traces)
+        dut.store_traces = True
+        self.assertTrue(dut.store_traces)
+
+    def test_read_reasponse(self, mock_log):
+        dut = Dut("test")
+        with mock.patch.object(dut, "readline") as mocked_readline:
+            mocked_readline.side_effect = ["retcode: 0", RuntimeError, "retcode: 0", "test"]
+            resp = dut._read_response()
+            self.assertTrue(isinstance(resp, CliResponse))
+            self.assertEqual(dut._read_response(), -1)
+            prior_len = len(dut.traces)
+            dut.store_traces = False
+            dut._read_response()
+            self.assertEqual(len(dut.traces), prior_len)
+            dut.store_traces = True
+            resp = dut._read_response()
+            self.assertEqual(len(dut.traces), prior_len + 1)
+            self.assertIsNone(resp)
 
 
 if __name__ == '__main__':
