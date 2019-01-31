37dc6a9939e9c47e53e09159da16412c55353c99
Joonas Nikula <joonas.nikula@arm.com>
2019-01-31 08:47:23 +0200

    Split DutMbed from DutSerial. Support generic serial duts. (#81)

--- icetea_lib/Plugin/plugins/LocalAllocator/LocalAllocator.py
+++ icetea_lib/Plugin/plugins/LocalAllocator/LocalAllocator.py
@@ -14,7 +14,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-
 LocalAllocator module. Implements allocating local resources using mbedls
 """
 
@@ -30,9 +29,50 @@ from icetea_lib.Plugin.plugins.LocalAllocator.DutDetection import DutDetection
 from icetea_lib.Plugin.plugins.LocalAllocator.DutConsole import DutConsole
 from icetea_lib.Plugin.plugins.LocalAllocator.DutProcess import DutProcess
 from icetea_lib.Plugin.plugins.LocalAllocator.DutSerial import DutSerial
+from icetea_lib.Plugin.plugins.LocalAllocator.DutMbed import DutMbed
 
 
-def init_hardware_dut(contextlist, conf, index, args):
+def init_generic_serial_dut(contextlist, conf, index, args):
+    """
+    Initializes a local hardware dut
+    """
+    port = conf['serial_port']
+    baudrate = (args.baudrate if args.baudrate else conf.get(
+        "application", {}).get("baudrate", 115200))
+    serial_config = {}
+    if args.serial_rtscts:
+        serial_config["serial_rtscts"] = args.serial_rtscts
+    elif args.serial_xonxoff:
+        serial_config["serial_xonxoff"] = args.serial_xonxoff
+
+    if args.serial_timeout:
+        serial_config["serial_timeout"] = args.serial_timeout
+
+    ch_mode_config = {}
+    if args.serial_ch_size > 0:
+        ch_mode_config["ch_mode"] = True
+        ch_mode_config["ch_mode_chunk_size"] = args.serial_ch_size
+    elif args.serial_ch_size is 0:
+        ch_mode_config["ch_mode"] = False
+
+    if args.ch_mode_ch_delay:
+        ch_mode_config["ch_mode_ch_delay"] = args.ch_mode_ch_delay
+    dut = DutSerial(name="D%d" % index, port=port, baudrate=baudrate,
+                    config=conf, ch_mode_config=ch_mode_config,
+                    serial_config=serial_config, params=args)
+    dut.index = index
+
+    dut.platform = conf.get("platform_name", "serial")
+
+    msg = 'Use device in serial port {} as D{}'
+    contextlist.logger.info(msg.format(port,
+                                       index))
+
+    contextlist.duts.append(dut)
+    contextlist.dutinformations.append(dut.get_info())
+
+
+def init_mbed_dut(contextlist, conf, index, args):
     """
     Initializes a local hardware dut
     """
@@ -68,9 +108,9 @@ def init_hardware_dut(contextlist, conf, index, args):
     if args.ch_mode_ch_delay:
         ch_mode_config["ch_mode_ch_delay"] = args.ch_mode_ch_delay
 
-    dut = DutSerial(name="D%d" % index, port=port, baudrate=baudrate,
-                    config=conf, ch_mode_config=ch_mode_config,
-                    serial_config=serial_config, params=args)
+    dut = DutMbed(name="D%d" % index, port=port, baudrate=baudrate,
+                  config=conf, ch_mode_config=ch_mode_config,
+                  serial_config=serial_config, params=args)
     dut.index = index
 
     # If something to flash, get the allocated device and flash it
@@ -78,7 +118,7 @@ def init_hardware_dut(contextlist, conf, index, args):
         if contextlist.check_flashing_need('hardware',
                                            binary,
                                            args.forceflash):
-            if dut.flash(binary=binary, forceflash=args.forceflash):
+            if dut.flash(binary_location=binary, forceflash=args.forceflash):
                 contextlist.logger.info('flash ready')
             else:
                 dut.close_dut(False)
@@ -178,7 +218,7 @@ class LocalAllocator(BaseAllocator):
         :return: True if type is supported, False otherwise
         """
         try:
-            return dut_configuration["type"] in ["hardware", "process"]
+            return dut_configuration["type"] in ["hardware", "process", "serial", "mbed"]
         except KeyError:
             return False
 
@@ -206,7 +246,7 @@ class LocalAllocator(BaseAllocator):
             for dut_config in dut_config_list:
                 if not self.can_allocate(dut_config.get_requirements()):
                     raise AllocationError("Resource type is not supported")
-                self._allocate(dut_config.get_requirements())
+                self._allocate(dut_config)
         except AllocationError:
             # Locally allocated don't need to be released any way for
             # now, so just re-raise the error
@@ -214,38 +254,44 @@ class LocalAllocator(BaseAllocator):
         alloc_list = AllocationContextList()
         res_id = None
         for conf in dut_config_list:
-            if conf.get("type") == "hardware":
+            if conf.get("type") == "mbed":
                 res_id = conf.get("allocated").get("target_id")
             context = AllocationContext(resource_id=res_id, alloc_data=conf)
             alloc_list.append(context)
 
-        alloc_list.set_dut_init_function("hardware", init_hardware_dut)
+        alloc_list.set_dut_init_function("serial", init_generic_serial_dut)
         alloc_list.set_dut_init_function("process", init_process_dut)
+        alloc_list.set_dut_init_function("mbed", init_mbed_dut)
 
         return alloc_list
 
     def release(self, dut=None):
         """
-        Resource releasing is not necessary. Not implemented
+        Resource releasing is not necessary. Not implemented.
+
         :param dut: Not used
         :return: Nothing
         """
         pass
 
-    def _allocate(self, dut_configuration):
+    def _allocate(self, dut_configuration):  # pylint: disable=too-many-branches
         """
-        Internal allocation function. Allocates a single resource based on dut_configuration
-        :param dut_configuration: Dictionary which describes a required resource
+        Internal allocation function. Allocates a single resource based on dut_configuration.
+
+        :param dut_configuration: ResourceRequirements object which describes a required resource
         :return: True
         :raises: AllocationError if suitable resource was not found or if the platform was not
         allowed to be used.
         """
         if dut_configuration["type"] == "hardware":
+            dut_configuration.set("type", "mbed")
+        if dut_configuration["type"] == "mbed":
             if not self._available_devices:
                 raise AllocationError("No available devices to allocate from")
-            platforms = None if 'allowed_platforms' not in dut_configuration else dut_configuration[
+            dut_reqs = dut_configuration.get_requirements()
+            platforms = None if 'allowed_platforms' not in dut_reqs else dut_reqs[
                 'allowed_platforms']
-            platform_name = None if 'platform_name' not in dut_configuration else dut_configuration[
+            platform_name = None if 'platform_name' not in dut_reqs else dut_reqs[
                 "platform_name"]
             if platform_name is None and platforms:
                 platform_name = platforms[0]
@@ -266,7 +312,7 @@ class LocalAllocator(BaseAllocator):
 
                 if DutDetection.is_port_usable(dev['serial_port']):
                     dev['state'] = "allocated"
-                    dut_configuration['allocated'] = dev
+                    dut_reqs['allocated'] = dev
                     self.logger.info("Allocated device %s", dev['target_id'])
                     return True
                 else:
@@ -274,6 +320,12 @@ class LocalAllocator(BaseAllocator):
                                      "allocated device %s", dev['serial_port'], dev['target_id'])
             # Didn't find a matching device to allocate so allocation failed
             raise AllocationError("No suitable local device available")
+        elif dut_configuration["type"] == "serial":
+            dut_reqs = dut_configuration.get_requirements()
+            if not dut_reqs.get("serial_port"):
+                raise AllocationError("Serial port not defined for requirement {}".format(dut_reqs))
+            if not DutDetection.is_port_usable(dut_reqs['serial_port']):
+                raise AllocationError("Serial port {} not usable".format(dut_reqs['serial_port']))
         # Successful allocation, return True
 
         return True
--- icetea_lib/Plugin/plugins/plugin_tests/test_dutserial.py
+++ icetea_lib/Plugin/plugins/plugin_tests/test_dutserial.py
@@ -1,5 +1,4 @@
-# pylint: disable=unused-argument,missing-docstring,too-many-arguments,too-few-public-methods
-# pylint: disable=no-self-use,protected-access,invalid-name
+# pylint: disable=unused-argument,invalid-name,missing-docstring,no-self-use
 
 """
 Copyright 2017 ARM Limited
@@ -14,172 +13,145 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+# pylint: disable=unused-argument,invalid-name,missing-docstring,no-self-use
+
 import unittest
 import mock
+from serial import SerialException
 
 from icetea_lib.Plugin.plugins.LocalAllocator.DutSerial import DutSerial
 from icetea_lib.DeviceConnectors.Dut import DutConnectionError
-
-try:
-    from mbed_flasher.common import FlashError
-except ImportError:
-    FlashError = None
+from icetea_lib.DeviceConnectors.DutInformation import DutInformation
 
 
-class MockArgspec(object):
-    def __init__(self, lst):
-        self.args = lst
-
-
+@mock.patch("icetea_lib.DeviceConnectors.Dut.LogManager")
 class DutSerialTestcase(unittest.TestCase):
 
-    # Mock base class
-    @mock.patch("icetea_lib.DeviceConnectors.Dut.LogManager.get_bench_logger")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.getargspec")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.get_external_logger")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.Flash")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.Build")
-    def test_flasher_logger_insert(self, mock_build, mock_flasher, mocked_logger, mock_inspect,
-                                   mock_bench_logger):
-        mock_inspect.return_value = MockArgspec(["logger"])
-        mocked_logger_for_flasher = mock.MagicMock()
-        mocked_logger.return_value = mocked_logger_for_flasher
-        dut = DutSerial(port="test",
-                        config={"allocated": {"target_id": "thing"}, "application": "thing"})
-        dut.flash("this_is_not_a_binary")
-        mock_flasher.assert_called_with(logger=mocked_logger_for_flasher)
+    def test_properties(self, mock_logger):
+        ds = DutSerial()
+        self.assertFalse(ds.ch_mode)
+        ds.ch_mode = True
+        self.assertTrue(ds.ch_mode)
 
-        mock_flasher.reset_mock()
-        mock_inspect.return_value = MockArgspec([])
-        dut = DutSerial(port="test",
-                        config={"allocated": {"target_id": "thing"}, "application": "thing"})
-        dut.flash("this_is_not_a_binary")
-        mock_flasher.assert_called_with()
+        self.assertEqual(ds.ch_mode_chunk_size, 1)
+        ds.ch_mode_chunk_size = 2
+        self.assertEqual(ds.ch_mode_chunk_size, 2)
 
-    @mock.patch("icetea_lib.DeviceConnectors.Dut.LogManager.get_bench_logger")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.getargspec")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.get_external_logger")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.Flash")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.Build")
-    def test_flash_build_init_not_implemented(self, mock_build, mock_flasher, mocked_logger,
-                                              mock_inspect, mock_bench_logger):
-        mock_build.init = mock.MagicMock(side_effect=[NotImplementedError])
-        mock_inspect.return_value = MockArgspec(["logger"])
-        mocked_logger_for_flasher = mock.MagicMock()
-        mocked_logger.return_value = mocked_logger_for_flasher
-        dut = DutSerial(port="test",
-                        config={"allocated": {"target_id": "thing"}, "application": "thing"})
+        self.assertEqual(ds.ch_mode_ch_delay, 0.01)
+        ds.ch_mode_ch_delay = 0.02
+        self.assertEqual(ds.ch_mode_ch_delay, 0.02)
+
+        self.assertEqual(ds.ch_mode_start_delay, 0)
+        ds.ch_mode_start_delay = 1
+        self.assertEqual(ds.ch_mode_start_delay, 1)
+
+        self.assertEqual(ds.serial_baudrate, 460800)
+        ds.serial_baudrate = 9600
+        self.assertEqual(ds.serial_baudrate, 9600)
+
+        self.assertEqual(ds.serial_timeout, 0.01)
+        ds.serial_timeout = 0.02
+        self.assertEqual(ds.serial_timeout, 0.02)
+
+        self.assertFalse(ds.serial_xonxoff)
+        ds.serial_xonxoff = True
+        self.assertTrue(ds.serial_xonxoff)
+
+        self.assertFalse(ds.serial_rtscts)
+        ds.serial_rtscts = True
+        self.assertTrue(ds.serial_rtscts)
+
+    def test_get_resource_id(self, mock_logger):
+        ds = DutSerial(config={"allocated": {"target_id": "test"}, "application": {}})
+        self.assertEqual(ds.get_resource_id(), "test")
+
+    def test_get_info(self, mock_logger):
+        ds = DutSerial()
+        info = ds.get_info()
+        self.assertTrue(isinstance(info, DutInformation))
+
+    def test_flash(self, mock_logger):
+        ds = DutSerial()
+        self.assertTrue(ds.flash())
+
+    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.EnhancedSerial")
+    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.Thread")
+    def test_open_connection(self, mock_thread, mock_es, mock_logger):
+        ds = DutSerial()
+        ds.readthread = 1
+        ds.params = mock.MagicMock()
+        type(ds.params).reset = mock.PropertyMock(return_value=False)
         with self.assertRaises(DutConnectionError):
-            dut.flash("try")
-
-    @mock.patch("icetea_lib.DeviceConnectors.Dut.LogManager.get_bench_logger")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.getargspec")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.get_external_logger")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.Flash")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.Build")
-    def test_flash_build_get_file_fail(self, mock_build, mock_flasher, mocked_logger, mock_inspect,
-                                       mock_bench_logger):
-        mock_build_object = mock.MagicMock()
-        mock_build_object.get_file = mock.MagicMock(side_effect=[False, "Thisbin"])
-        mock_build.init = mock.MagicMock(return_value=mock_build_object)
-        mock_inspect.return_value = MockArgspec(["logger"])
-        mocked_logger_for_flasher = mock.MagicMock()
-        mocked_logger.return_value = mocked_logger_for_flasher
-        dut = DutSerial(port="test",
-                        config={"allocated": {"target_id": "thing"}, "application": "thing"})
+            ds.open_connection()
+        ds.readthread = None
+        mock_es_2 = mock.MagicMock()
+        mock_es_2.flushInput = mock.MagicMock(side_effect=[SerialException, ValueError, 1])
+        mock_es.return_value = mock_es_2
         with self.assertRaises(DutConnectionError):
-            dut.flash("try")
+            ds.open_connection()
+        with self.assertRaises(ValueError):
+            ds.open_connection()
+        ds.open_connection()
+        mock_thread.assert_called_once()
 
-    @mock.patch("icetea_lib.DeviceConnectors.Dut.LogManager.get_bench_logger")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.getargspec")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.get_external_logger")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.Flash")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.Build")
-    def test_flash_fails(self, mock_build, mock_flasher, mocked_logger, mock_inspect,
-                         mock_bench_logger):
-        mock_build_object = mock.MagicMock()
-        mock_build_object.get_file = mock.MagicMock(return_value="Thisbin")
-        mock_build.init = mock.MagicMock(return_value=mock_build_object)
-        mock_inspect.return_value = MockArgspec(["logger"])
-        mocked_logger_for_flasher = mock.MagicMock()
-        mocked_logger.return_value = mocked_logger_for_flasher
-        mock_flasher_object = mock.MagicMock()
-        mock_flasher.return_value = mock_flasher_object
-        mock_flasher_object.flash = mock.MagicMock()
-        mock_flasher_object.flash.side_effect = [NotImplementedError, SyntaxError, 1]
-        if FlashError is not None:
-            mock_flasher_object.flash.side_effect = [NotImplementedError, SyntaxError, 1,
-                                                     FlashError("Error", 10)]
-        dut = DutSerial(port="test",
-                        config={"allocated": {"target_id": "thing"}, "application": "thing"})
-        with self.assertRaises(DutConnectionError):
-            dut.flash("try")
-        with self.assertRaises(DutConnectionError):
-            dut.flash("try2")
-        self.assertFalse(dut.flash("try3"))
-        if FlashError is not None:
-            with self.assertRaises(DutConnectionError):
-                dut.flash("try4")
+    def test_prepare_connection_close(self, mock_logger):
+        ds = DutSerial()
+        with mock.patch.object(ds, "init_cli_human") as mock_init_cli_human:
+            with mock.patch.object(ds, "stop") as mock_stop:
+                ds.prepare_connection_close()
+                mock_init_cli_human.assert_called_once()
+                mock_stop.assert_called_once()
 
-    @mock.patch("icetea_lib.DeviceConnectors.Dut.LogManager.get_bench_logger")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.getargspec")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.get_external_logger")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.Flash")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.Build")
-    def test_flash_skip_flash(self, mock_build, mock_flasher, mocked_logger, mock_inspect,
-                              mock_bench_logger):
-        mock_build_object = mock.MagicMock()
-        mock_build_object.get_file = mock.MagicMock(return_value="Thisbin")
-        mock_build.init = mock.MagicMock(return_value=mock_build_object)
-        mock_inspect.return_value = MockArgspec(["logger"])
-        mock_logger_for_flasher = mock.MagicMock()
-        mocked_logger.return_value = mock_logger_for_flasher
-        mock_flasher_object = mock.MagicMock()
-        mock_flasher.return_value = mock_flasher_object
-        mock_flasher_object.flash = mock.MagicMock()
-        mock_flasher_object.flash.return_value = 0
-        dut = DutSerial(port="test", config={
-            "allocated": {"target_id": "thing"},
-            "application": "thing"
-        })
-        self.assertTrue(dut.flash("try"))
-        self.assertTrue(dut.flash("try"))
-        self.assertTrue(dut.flash("try"))
-        self.assertEqual(mock_flasher_object.flash.call_count, 1)
-        self.assertTrue(dut.flash("try", forceflash=True))
-        self.assertEqual(mock_flasher_object.flash.call_count, 2)
+    def test_close_connection(self, mock_logger):
+        ds = DutSerial()
+        mocked_port = mock.MagicMock()
+        mocked_port.close = mock.MagicMock()
+        ds.port = mocked_port
+        with mock.patch.object(ds, "stop") as mock_stop:
+            ds.close_connection()
+            mock_stop.assert_called_once()
+            mocked_port.close.assert_called_once()
+            self.assertFalse(ds.port)
 
-    @mock.patch("icetea_lib.DeviceConnectors.Dut.LogManager.get_bench_logger")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.getargspec")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.get_external_logger")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.Flash")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.Build")
-    def test_peek(self, mock_build, mock_flasher, mocked_logger, mock_inspect,
-                  mock_bench_logger):
-        dut = DutSerial(port="test", config={
-            "allocated": {"target_id": "thing"},
-            "application": "thing"
-        })
-        dut.port = mock.MagicMock()
-        dut.port.peek = mock.MagicMock(return_value="test")
-        self.assertEqual(dut.peek(), "test")
+    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.time")
+    def test_reset(self, mocked_time, mock_logger):
+        ds = DutSerial()
+        mock_port = mock.MagicMock()
+        mock_port.safe_sendBreak = mock.MagicMock(return_value=True)
+        mocked_time.sleep = mock.MagicMock()
+        ds.port = mock_port
+        self.assertIsNone(ds.reset())
 
-    @mock.patch("icetea_lib.DeviceConnectors.Dut.LogManager.get_bench_logger")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.getargspec")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.get_external_logger")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.Flash")
-    @mock.patch("icetea_lib.Plugin.plugins.LocalAllocator.DutSerial.Build")
-    def test_readline(self, mock_build, mock_flasher, mocked_logger, mock_inspect,
-                      mock_bench_logger):
-        dut = DutSerial(port="test", config={
-            "allocated": {"target_id": "thing"},
-            "application": "thing"
-        })
-        dut.port = mock.MagicMock()
-        dut.port.readline = mock.MagicMock(side_effect=["test\n", None])
-        self.assertEqual(dut._readline(), "test")
-        self.assertIsNone(dut._readline())
+    def test_writeline(self, mock_logger):
+        ds = DutSerial()
+        mock_port = mock.MagicMock()
+        mock_port.write = mock.MagicMock(side_effect=[1, 1, 1, 1, SerialException])
+        ds.port = mock_port
+        ds.ch_mode = False
+        ds.writeline("data")
+        mock_port.write.assert_called_once_with("data\n".encode())
+        mock_port.reset_mock()
+        ds.ch_mode = True
+        ds.ch_mode_chunk_size = 2
+        ds.writeline("data")
+        mock_port.write.assert_has_calls([mock.call("da".encode()), mock.call("ta".encode()),
+                                          mock.call("\n".encode())])
+        with self.assertRaises(RuntimeError):
+            ds.writeline("data")
 
+    def test_readline(self, mock_logger):
+        ds = DutSerial()
+        ds.input_queue.append("test1")
+        read = ds.readline()
+        self.assertEqual(read, "test1")
+        self.assertIsNone(ds.readline())
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_peek(self, mock_logger):
+        ds = DutSerial()
+        mocked_port = mock.MagicMock()
+        mocked_port.peek = mock.MagicMock(return_value="test")
+        ds.port = mocked_port
+        self.assertEqual(ds.peek(), "test")
+        mocked_port.peek.assert_called_once()
+        ds.port = None
+        self.assertEqual(ds.peek(), "")
