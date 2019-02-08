3e3f185fdfb948adb72c10e4076847555fd13878
Joonas Nikula <joonas.nikula@arm.com>
2019-02-08 13:49:16 +0200

    Some fixup.

--- test/tests/matching_test/feature1_test.py
+++ test/tests/matching_test/feature1_test.py
@@ -1,5 +1,23 @@
 # pylint: disable=missing-docstring
 
+"""
+Copyright 2017 ARM Limited
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Testcase for Testcase and Bench, test teardown with invalid command sent to dut.
+Should fail in all cases and cause execution to skip case and go to from setup to teardown.
+"""
+
 from icetea_lib.bench import Bench
 
 
