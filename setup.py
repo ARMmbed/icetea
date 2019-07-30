d727997f206b6b390959ed4352d45c38c7796c79
Joonas Nikula <joonas.nikula@arm.com>
2019-07-30 15:36:49 +0300

    Version number update

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools.command.install import install
 DESCRIPTION = "Icetea - test framework"
 OWNER_NAMES = "Joonas Nikula"
 OWNER_EMAILS = "joonas.nikula@arm.com"
-VERSION = "2.0.0"
+VERSION = "2.0.1"
 
 
 def read(fname):
